533b507bb284fa57de83ad031983e8a5308935f1
AntonioFernando0 <jufer376@gmail.com>
2024-09-16 11:02:48 -0300

    Bootstrap
    Aprendendo a utilizar Bootstrap

--- index.js
+++ index.js
@@ -11,30 +11,17 @@ app.set ('view engine', 'ejs')
 app.use (express.static('public'))
 
 
-app.get ("/:nome/:lang",(req, res) => {
-    let nome = req.params.nome
-    let lang = req.params.lang 
-    let exibirmsg = false
+app.get ("/",(req, res) => {
+    
+    res.render("index")
+        
 
+   
+})
 
-    let produtos = [
+app.get("/perguntar", (req, res)=> {
+    res.render("perguntar")
 
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Cola", preco: 5.10},
-        {nome: "Carne", preco: 40.25},
-        {nome: "RedBull", preco: 10.00},
-        {nome: "Água", preco: 3.50},
-        {nome: "Ovos", preco: 1.05}
-    ]
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Fernando Santana",
-        inscritos: 10000,
-        msg: exibirmsg,
-        produtos: produtos
-
-    })
 })
 
 app.listen ( 80, () => {
