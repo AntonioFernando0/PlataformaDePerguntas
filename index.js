1ee7a261f977d90ada671aaaf8b2db85858eea26
AntonioFernando0 <jufer376@gmail.com>
2024-09-19 13:58:16 -0300

    Guia de perguntas
    Utilizando o bodyparser.

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-const express = require ("express")
-const app = express()
-require('events').EventEmitter.defaultMaxListeners = 15;
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser")
+
+// Configuração do EJS como view engine
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+// Express já suporta o parsing de body nativamente (não é necessário body-parser)
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
+// Rotas
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
-// Estou dizendo para o Express usar o EJS como view enginer
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+});
 
-app.set ('view engine', 'ejs')
+// Rota para processar o formulário
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! Título: " + titulo + " Descrição: " + descricao);
+});
 
-app.use (express.static('public'))
 
-
-app.get ("/",(req, res) => {
-    
-    res.render("index")
-        
-
-   
-})
-
-app.get("/perguntar", (req, res)=> {
-    res.render("perguntar")
-
-})
-
-app.listen ( 80, () => {
-    console.log(" App rodando!")
-})
+app.listen(80, () => {
+    console.log("App rodando!");
+});
